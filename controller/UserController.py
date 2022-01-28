9916de3745aca72fe0460ea01a2f16f680ee1770
abiz95 <abintitusjohn@gmail.com>
2022-01-28 20:22:26 +0530

    logging added to usercontroller

--- controller/UserController.py
+++ controller/UserController.py
@@ -10,18 +10,20 @@ def getAllData():
     try:
         if request.method == 'GET':
             data = getData("SELECT * FROM User")
-            app.logger.info('Info level log')
+            app.logger.info('[UserController][getAllData] getting all the data')
             response = jsonify(data)
             response.status_code = HttpStatus.OK
             return response
         else:
             ResponseData.construct['success'] = False
             ResponseData.construct['error'] = 'Something happened. Please try again!'
+            app.logger.error('[UserController][getAllData] request failed')
             response = jsonify(ResponseData.construct)
             response.status_code = HttpStatus.BAD_REQUEST
             return response
     except Exception as e:
         print(e)
+        app.logger.error('[UserController][getAllData] an exception occurred: ', e)
         ResponseData.construct['success'] = False
         ResponseData.construct['error'] = str(e)
         response.status_code = HttpStatus.BAD_REQUEST
@@ -38,6 +40,7 @@ def saveDatas():
         password = json['password']
         username = json['username']
         if email and password and username and request.method == 'POST':
+            app.logger.info('[UserController][saveDatas] inserting user data')
             sql = "INSERT INTO user(email, password, username) VALUES( %s, %s, %s)"
             data = (email, password, username)
             response = saveData(sql, data)
@@ -47,6 +50,7 @@ def saveDatas():
         else:
             ResponseData.construct['success'] = False
             ResponseData.construct['error'] = 'Something happened. Please try again!'
+            app.logger.error('[UserController][saveDatas] request failed')
             response = jsonify(ResponseData.construct)
             response.status_code = HttpStatus.BAD_REQUEST
             return response
@@ -54,6 +58,7 @@ def saveDatas():
         print(e)
         ResponseData.construct['success'] = False
         ResponseData.construct['error'] = str(e)
+        app.logger.error('[UserController][saveDatas] an exception occurred: ', e)
         response = jsonify(ResponseData.construct)
         response.status_code = HttpStatus.BAD_REQUEST
         return response
@@ -68,6 +73,7 @@ def updateData(id):
         username = json['username']
         if email and password and username and request.method == 'PUT':
             sql = "UPDATE user SET email = %s, password = %s, username = %s WHERE username = %s"
+            app.logger.info('[UserController][updateData] updating user data of the user ', id)
             data = (email, password, username, id)
             response = saveData(sql, data)
             response = jsonify(response)
@@ -76,6 +82,7 @@ def updateData(id):
         else:
             ResponseData.construct['success'] = False
             ResponseData.construct['error'] = 'Something happened. Please try again!'
+            app.logger.error('[UserController][updateData] request failed')
             response = jsonify(ResponseData.construct)
             response.status_code = HttpStatus.BAD_REQUEST
             return response
@@ -83,6 +90,7 @@ def updateData(id):
         print(e)
         ResponseData.construct['success'] = False
         ResponseData.construct['error'] = str(e)
+        app.logger.error('[UserController][updateData] an exception occurred: ', e)
         response = jsonify(ResponseData.construct)
         response.status_code = HttpStatus.BAD_REQUEST
         return response
@@ -92,6 +100,7 @@ def updateData(id):
 def deleteData(id):
     try:
         if request.method == 'DELETE':
+            app.logger.info('[UserController][deleteData] deleting user data of ', id)
             sql = "DELETE FROM user WHERE username = %s"
             response = saveData(sql, id)
             response = jsonify(response)
@@ -100,6 +109,7 @@ def deleteData(id):
         else:
             ResponseData.construct['success'] = False
             ResponseData.construct['error'] = 'Something happened. Please try again!'
+            app.logger.error('[UserController][deleteData] request failed')
             response = jsonify(ResponseData.construct)
             response.status_code = HttpStatus.BAD_REQUEST
             return response
@@ -107,6 +117,7 @@ def deleteData(id):
         print(e)
         ResponseData.construct['success'] = False
         ResponseData.construct['error'] = str(e)
+        app.logger.error('[UserController][deleteData] an exception occurred: ', e)
         response = jsonify(ResponseData.construct)
         response.status_code = HttpStatus.BAD_REQUEST
         return response
